f5a799580a3662c393472f7fa6b2492badca810a
Helen <hkg0017@auburn.edu>
2026-02-19 22:28:36 -0600

    Updated Question/Answer Excel Sheet

--- src/scenes/GM3Level1.js
+++ src/scenes/GM3Level1.js
@@ -12,7 +12,7 @@ export default class GM3Level1 extends BaseGM3Scene {
   }
 
   preload() {
-    this.load.binary("gm3_easy_xlsx", "assets/UpdatedAccountingElements.xlsx");
+    this.load.binary("gm3_easy_xlsx", "assets/UpdatedAccountingElements_v2.26.xlsx");
     this.load.image("gm3_level1_bg", "assets/level1.jpg");
   }
 
