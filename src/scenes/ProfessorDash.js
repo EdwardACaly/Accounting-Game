44f2f63a339e0cff1487ea5505bb16f9da5e06fe
Cole Suchan <cdsuchan@gmail.com>
2026-04-08 06:31:10 -0500

    Unverified dashboard changes PLEASE check

--- src/scenes/ProfessorDash.js
+++ src/scenes/ProfessorDash.js
@@ -8,7 +8,7 @@ export default class ProfessorDash extends Scene {
     }
 
     create() {
-        this.add.rectangle(0, 0, this.scale.width, this.scale.height, 0x1a1a1a).setOrigin(0);
+        this.add.rectangle(0, 0, this.scale.width, this.scale.height, 0x550000).setOrigin(0);
 
         this.title = this.add.text(this.scale.width / 2, 50, "Professor Dashboard", {
             fontSize: "42px", fontFamily: '"Jersey 10", sans-serif', color: "#dcc89f"
@@ -58,10 +58,10 @@ export default class ProfessorDash extends Scene {
 
             data.student_breakdown.forEach((s) => {
                 // Formatting to include Top and Bottom
-                const row = `${s.name.padEnd(15)} | ${s.game.padEnd(8)} | Avg: ${s.avg.toFixed(0).padStart(4)} | T: ${String(s.top).padStart(4)} | B: ${String(s.bottom).padStart(4)}`;
+                const row = `${s.name.padEnd(15)} | ${s.game.padEnd(8)} | Avg: ${s.avg.toFixed(0).padStart(4)} | T: ${String(s.top).padStart(4)} | B: ${String(s.bottom).padStart(4)} | Time: ${String(s.time_played).padStart(4)}s`;
     
                 let txt = this.add.text(0, yOffset, row, {
-                    fontSize: "16px", // Slightly smaller to fit the extra data
+                    fontSize: "14px", // Slightly smaller to fit the extra data
                     fontFamily: "Courier", 
                     color: "#ffffff"
                 }).setOrigin(0.5);
